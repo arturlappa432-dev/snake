9cdbbec0858cb1b7a40cae2ac5f07f5f1803e764
arturlappa432-dev <artur.lappa432@gmail.com>
2025-11-10 08:47:48 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'snake-cache-v1';
+const urlsToCache = [
+  'index.html',
+  'music/menu.mp3',
+  'sounds/apple.wav',
+  'sw.js'
+];
+
+
+// Установка SW и кеширование всех файлов
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Активация SW
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Перехват запросов: отдаём из кеша или с сервера
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request)
+        .then((networkResponse) => {
+          // Кешируем новые файлы динамически
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        })
+        .catch(() => {
+          // Если нет сети и файла в кеше, можно вернуть оффлайн-страницу (по желанию)
+          // return caches.match('/offline.html');
+        });
+    })
+  );
+});
